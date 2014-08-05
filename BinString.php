fc619d3eb291cd953166e0143c43ca2b3137622f
Asbjorn Grandt <asbjorn@grandt.com>
2014-08-05 08:21:17 +0200

    Stepping to version 0.2.0

--- BinString.php
+++ BinString.php
@@ -1,69 +1,19 @@
 <?php
 /**
  * If you use mbstring.func_overload, or the server you are running on has it enabled, you are in trouble.
+ * This class will help overcome that, by provide alternative functions to work around it.
  *
- * To the question "Should I use multi-byte overloading (mbstring.func_overload)?". user 'gphilip' said it well on this
- * StackOverflow post:
- * http://stackoverflow.com/questions/222630/should-i-use-multi-byte-overloading-mbstring-func-overload
- *
- *   > My answer is: definitely not!
- *   >
- *   > The problem is that there is no easy way to "reset"
- *   > str* functions once they are overloaded.
- *   >
- *   > For some time this can work well with your project,
- *   > but almost surely you will run into an external library
- *   > that uses string functions to, for example, implement a
- *   > binary protocol, and they will fail. They will fail and
- *   > you will spend hours trying to find out why they are
- *   > failing.
- *
- * This class is a wrapper for string functions, in cases where the mbstring.func_overload tripe have been enabled.
- * Be warned, use this class ONLY if you have to, as it *will* affect performance a bit. For some functions, a lot,
- * though that is due to problems in mb_string, not this class.
- * Function calls in PHP are fairly expensive on their own, and if func_overload is enabled, it'll use mb_string
- * functions exclusively in place of the built-in PHP string, to parse them as 'latin1', which is also expensive, cpu
- * wise.
- *
- * PHP, like Java, have length aware strings, meaning the object header knows how long your string is. They are binary
- * safe, and not null (0x00) terminated.
- *
- * mb_string functions ignore that, and parse the entirety of the string, to figure out what is what. stelen(string)
- * simply tells you how many bytes are in it, mb_strlen will parse it, to find multi byte characters, and tell you
- * how many characters there are. That is great for handling multi-byte encoded strings correctly, such as UTF-8, it
- * sucks for binary data handling, as multi-byte sequences are bound to occur by random chance, in any large enough
- * binary data set.
- *
- * The functions overloaded by mbstring is:
- *  mbstring.func_overload
- *    value    original function    overloaded function
- *        1    mail()               mb_send_mail()
- *
- *        2    strlen()             mb_strlen()
- *        2    strpos()             mb_strpos()
- *        2    strrpos()            mb_strrpos()
- *        2    substr()             mb_substr()
- *        2    strtolower()         mb_strtolower()
- *        2    strtoupper()         mb_strtoupper()
- *        2    substr_count()       mb_substr_count()
- *
- *        4    ereg()               mb_ereg()
- *        4    eregi()              mb_eregi()
- *        4    ereg_replace()       mb_ereg_replace()
- *        4    eregi_replace()      mb_eregi_replace()
- *        4    split()              mb_split()
- *
- * License: GNU LGPL 2.1.
+ * see readme.markdown for further details.
  *
  * @author A. Grandt <php@grandt.com>
  * @copyright 2014 A. Grandt
  * @license GNU LGPL 2.1
- * @version 0.10
+ * @version 0.20
  */
 namespace com\grandt;
 
 class BinString {
-    const VERSION = 0.10;
+    const VERSION = 0.20;
 
     private $has_mbstring = FALSE;
     private $has_mb_shadow = FALSE;
